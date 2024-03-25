1c052b8840c2c3194a969ba68d6ce9af6a884ec8
oussama khelaifa <khlaifioussama27@gmail.com>
2024-03-25 15:16:21 +0100

    .

--- model/api.php
+++ model/api.php
@@ -1,5 +1,6 @@
 <?php
 require_once "../controller/bdd.php";
+$pdo = get_PDO();
 // Supposons que $products soit un tableau associatif contenant les données des produits
 
 // Conversion en JSON et envoi de la réponse
